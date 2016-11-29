2618b4594c925f25875387c4a62ff6619da3f89e
Bruno Luis Panuto Silva <bl.panuto@gmail.com>
2016-11-29 21:05:05 -0200

    Fix shitty express static asset serving

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.use('/static', express.static(__dirname + '/css'));
-app.use('/static', express.static(__dirname + '/js'));
+app.use(express.static('public'));
 
 app.listen(port, function() {
     console.log(`Listening on port ${port}`);
